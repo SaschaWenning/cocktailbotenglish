1ea9270d0ea082923ead2c39160f92d4d5010ea2
SaschaWenning <135637184+SaschaWenning@users.noreply.github.com>
2025-06-24 12:30:52 +0200

    Add files via upload

--- data/cocktails.ts
+++ data/cocktails.ts
@@ -278,7 +278,7 @@ export const cocktails: Cocktail[] = [
     id: "mai-tai",
     name: "Mai Tai",
     description: "Classic Tiki cocktail with dark rum and almond syrup",
-    image: "/images/cocktails/mai_tai.jpg",
+    image: "/images/cocktails/Mai_Tai.jpg",
     alcoholic: true,
     ingredients: [
       "60ml Dark Rum",
